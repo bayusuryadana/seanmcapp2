f38b3fffc6baa115aaac1745ea22c4a24cf4537a
bayusuryadana <bayusuryadana@gmail.com>
2023-08-30 19:53:20 +0800

    hardcoded

--- database.py
+++ database.py
@@ -5,12 +5,13 @@ from functools import lru_cache
 class Database:
 
     def __init__(self, cfg: ConfigParser):
+        # port = cfg.get('Database', 'port', vars=os.environ)
         self.conn = psycopg2.connect(
             database = cfg.get('Database', 'database', vars=os.environ),
             host = cfg.get('Database', 'host', vars=os.environ),
             user = cfg.get('Database', 'user', vars=os.environ),
             password = cfg.get('Database', 'password', vars=os.environ),
-            port = cfg.get('Database', 'port', vars=os.environ)
+            port = 5432
         )
         self.cursor = self.conn.cursor()
 
